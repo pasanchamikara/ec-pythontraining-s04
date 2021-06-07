dd036bbd8cc6f06f25baf83661a5f7617f2cd343
Pasan Chamikara <gwpc92@gmail.com>
2021-06-07 06:11:02 +0530

    python training tutorial

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("mysql+mysqldb://root:pass@23.92.23.113/mydb", echo=True, pool_size=6, max_overflow=10, encoding='latin1')
-# engine = create_engine("mysql+pymysql://root:pass@localhost/mydb")
+# engine = create_engine("mysql+mysqldb://root@23.92.23.113/mydb", echo=True, pool_size=6, max_overflow=10, encoding='latin1')
+engine = create_engine("mysql+pymysql://root@localhost/ectest")
 
 engine.connect()
 
--- migrate.py
+++ migrate.py
@@ -1,5 +1,18 @@
 from config.dbconfig import engine
 
-from models.items import Items
-from models.users import Users
+from sqlalchemy import create_engine, Table, Integer, String, Column, Float
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+Base = declarative_base()
+    
+class Items(Base):
+   __tablename__ = 'items'
+   
+   id = Column(Integer, primary_key = True)
+   name = Column(String(50))
+   price = Column(Float)
+   description = Column(String(50))
+
+Base.metadata.create_all(engine)
 
